163edb7c3808361cc155bc1e04466cdac179be64
Failfer <dceaton9@gmail.com>
2025-06-20 16:41:53 -0700

    player doesnt leave the map on the right anymore

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ function queuePlayerMovement(event){
     if (event.key == "a"){
         playerMove = -1;
     }
+    if(event.code == "Space"){
+        playerMove = 0;
+    }
 
 }
 document.addEventListener("keydown", queuePlayerMovement);
@@ -17,10 +20,11 @@ document.addEventListener("keydown", queuePlayerMovement);
 
 function gameRunning(){    
     if(playerMove == 1){
-        playerBody.style.paddingLeft = parseInt(getComputedStyle(playerBody).paddingLeft, 10) + 222+ "px"
+        if(parseInt(getComputedStyle(playerBody).paddingLeft) <= 1700){
+            playerBody.style.paddingLeft = parseInt(getComputedStyle(playerBody).paddingLeft, 10) + 222+ "px"
+        }
     }
     if(playerMove == -1){
-        console.log(22222222222222222)
         playerBody.style.paddingLeft = parseInt(getComputedStyle(playerBody).paddingLeft, 10) - 222+ "px"
 
     }
